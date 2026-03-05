7d24242cd7a7a2d5f4812e9a8421e881e57c8f52
Daro <you@daro.com>
2026-03-05 12:10:07 +0100

    Adding connection with json path and loading data to mongoflix

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const dataPath = path.join(__dirname, '../data/filmy.json');
+const data = require(dataPath);
 
+
+db = db.getSiblingDB('mongoflix');
+
+db.filmy.drop();
+db.uzytkownicy.drop();
+db.recenzje.drop();
+
+print("--- Start importu danych do MongoFlix ---");
+
+
+db.filmy.insertMany(data.filmy);
+db.uzytkownicy.insertMany(data.uzytkownicy);
+db.recenzje.insertMany(data.recenzje);
+
+print("--- Sukces: Dane zostały zaimportowane do MongoFlix ---");
