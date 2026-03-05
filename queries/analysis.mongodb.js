020614a4095709f5d4482afee6cbbfc6131a8568
maciejs <maciej.sledzinski@cyfroweszkoly.pl>
2026-03-05 12:24:05 +0100

    Skrypt aktualizacji

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,1 +1,30 @@
+db = db.getSiblingDB('mongoflix');
 
+print("\n--- AKTUALIZACJA DANYCH FILMÓW ---");
+
+print("\n1. Oznaczanie popularnych filmów jako FEATURED:");
+
+const wynik = db.filmy.updateMany(
+    { licznik_odtworzen: { $gt: 2500 } },
+    { $set: { featured: true } }
+);
+
+print("Zaktualizowano filmów: " + wynik.modifiedCount);
+
+print("\n2. Oznaczanie pozostałych filmów:");
+
+const wynik2 = db.filmy.updateMany(
+    { licznik_odtworzen: { $lte: 2500 } },
+    { $set: { featured: false } }
+);
+
+print("Zaktualizowano filmów: " + wynik2.modifiedCount);
+
+print("\n3. Lista filmów FEATURED:");
+
+const featuredFilmy = db.filmy.find(
+    { featured: true },
+    { tytul: 1, licznik_odtworzen: 1, featured: 1, _id: 0 }
+).toArray();
+
+printjson(featuredFilmy);
